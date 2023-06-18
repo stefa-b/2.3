fd5dbaeafc60293883a53769a04b8910f1d65c33
Stefania <126661019+stefa-b@users.noreply.github.com>
2023-06-18 14:48:52 +0300

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-if __name__ == '__main__':
-    s = input("Введите предложение: ")
-    r = s.replace(' ', '_')
-    print(f"Предложение после замены: {r}")
